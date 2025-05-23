fd7984f0690c1c2996bcc7c42c73d1a92931e12f
Sage295 <se731266@ucf.edu>
2025-05-22 21:57:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
+window.onload = function () {
+  var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.isDeleting = false;
+    this.tick();
+  };
 
+  TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-function toggleMenu() {
-  var menu = document.getElementById("menu");
-  if (menu.style.display === "block") {
-      menu.style.display = "none";
-  } else {
-      menu.style.display = "block";
+    this.txt = this.isDeleting
+      ? fullTxt.substring(0, this.txt.length - 1)
+      : fullTxt.substring(0, this.txt.length + 1);
+
+    // Update text inside .wrap, do NOT replace .wrap element
+    if (this.el.querySelector('.wrap')) {
+      this.el.querySelector('.wrap').textContent = this.txt;
+    }
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) delta /= 2;
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
-}
 
-// Close menu when clicking outside
-document.addEventListener("click", function (event) {
-  var menu = document.getElementById("menu");
-  var icon = document.querySelector(".menu-icon");
+  // Scroll fade switch
+  let hasSwitched = false;
+  window.addEventListener("scroll", function () {
+    const portfolio = document.getElementById("portfolio");
+    const hello = document.getElementById("helloSejal");
 
-  if (!menu.contains(event.target) && !icon.contains(event.target)) {
-      menu.style.display = "none";
-  }
-});
+    const triggerPoint = window.innerHeight / 3;
+
+    if (!hasSwitched && window.scrollY > triggerPoint) {
+      portfolio.classList.remove("visible");
+      portfolio.classList.add("hidden");
+
+      hello.classList.remove("hidden");
+      hello.classList.add("visible");
+
+      hasSwitched = true;
+    }
+  });
+};
