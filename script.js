fc69b49dc5659d3c367feb77b3b25d5c9ef3629c
Sage295 <se731266@ucf.edu>
2025-05-25 22:51:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,80 @@
-window.onload = function () {
-  var TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.isDeleting = false;
-    this.tick();
-  };
-
-  TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    this.txt = this.isDeleting
-      ? fullTxt.substring(0, this.txt.length - 1)
-      : fullTxt.substring(0, this.txt.length + 1);
-
-    // Update text inside .wrap, do NOT replace .wrap element
-    if (this.el.querySelector('.wrap')) {
-      this.el.querySelector('.wrap').textContent = this.txt;
-    }
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) delta /= 2;
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  window.onload = function () {
+    // Typing effect
+    var TxtType = function (el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
       this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+      this.tick();
+    };
+
+    TxtType.prototype.tick = function () {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      this.txt = this.isDeleting
+        ? fullTxt.substring(0, this.txt.length - 1)
+        : fullTxt.substring(0, this.txt.length + 1);
+
+      if (this.el.querySelector('.wrap')) {
+        this.el.querySelector('.wrap').textContent = this.txt;
+      }
+
+      var that = this;
+      var delta = 200 - Math.random() * 100;
+
+      if (this.isDeleting) delta /= 2;
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function () {
+        that.tick();
+      }, delta);
+    };
+
+    // Start typing effect
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
     }
 
-    setTimeout(function () {
-      that.tick();
-    }, delta);
+    document.querySelectorAll(".toggle-button, .toggle-button2").forEach(button => {
+      button.addEventListener("click", () => {
+        const card = button.closest(".project-card-left");
+        const details = card.querySelector(".project-details");
+
+        // Toggle visibility only for this card
+        details.classList.toggle("hidden");
+      });
+    });
+
+    // Scroll listener for header
+    const header = document.getElementById("header");
+
+    if (header) {
+      window.addEventListener("scroll", () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop === 0) {
+          header.style.transform = "translateY(0)";
+          header.style.opacity = "1";
+        } else {
+          header.style.transform = "translateY(-100%)";
+          header.style.opacity = "0";
+        }
+      });
+    }
   };
-
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-type');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-
-  // Scroll fade switch
-  let hasSwitched = false;
-  window.addEventListener("scroll", function () {
-    const portfolio = document.getElementById("portfolio");
-    const hello = document.getElementById("helloSejal");
-
-    const triggerPoint = window.innerHeight / 3;
-
-    if (!hasSwitched && window.scrollY > triggerPoint) {
-      portfolio.classList.remove("visible");
-      portfolio.classList.add("hidden");
-
-      hello.classList.remove("hidden");
-      hello.classList.add("visible");
-
-      hasSwitched = true;
-    }
-  });
-};
